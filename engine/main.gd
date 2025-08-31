3a50f17db459d93562209f9989ca5beef5f76cd7
Substain <Dav.d@gmx.de>
2025-08-31 15:32:34 +0200

    add mouse wheel control

--- engine/main.gd
+++ engine/main.gd
@@ -83,6 +83,7 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("quit"):
 		ui.quit()
 
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton && (event as InputEventMouseButton).button_index == MOUSE_BUTTON_LEFT && (event as InputEventMouseButton).pressed:
 		do_continue()
